3cadd77074b3331b2871966ae458a909ad837cef
Paritosh Gavali <paritosh191200@gmail.com>
2021-03-13 14:54:39 +0530

    changed run.sh

--- lab6/run.sh
+++ lab6/run.sh
@@ -1,4 +1,5 @@
 cp ./open.c /usr/src/minix/minix/servers/vfs/open.c
 cp ./read.c /usr/src/minix//minix/servers/vfs/read.c
 cp ./link.c /usr/src/minix/minix/servers/vfs/link.c
+cd ..
 make build MKUPDATE=yes >log.txt 2>log.txt
